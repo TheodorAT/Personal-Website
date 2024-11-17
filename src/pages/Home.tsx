2225e85b1cad6bf83866340f9c59c3a688dd8f13
TheodorAT <theodor.anderberg.torngren@gmail.com>
2024-11-17 18:21:19 +0100

    Completed home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Container from "react-bootstrap/Container";
 import profilePic from "../assets/profile.jpg";
+import cv from "../assets/CV.pdf";
 import "../App.css";
 import "./Home.css";
 
@@ -7,26 +8,58 @@ function About() {
   return (
     <div className="mainDiv">
       <Container>
-        <div id="header">
+        <div className="section" id="header">
           <img className="profile" src={profilePic}></img>
           <div id="headertext">
             <h1>Theodor Anderberg Törngren</h1>
-            <p>This is some text about me</p>
+            <p>
+              I am a fifth-year student of Engineering Physics at LTH with a
+              specialization in Machine Learning. With several experiences of
+              working with software development in both industrial and academic
+              settings and a passion for solving difficult problems together and
+              understanding new concepts.
+            </p>
           </div>
         </div>
-        <div className="infoCards">
+        <div className="section infoCards">
           <div className="infoCard">
-            <p>5th year Engineering Physics Student at LTH</p>
+            <h1>5th</h1>
+            <p>Year Engineering Physics Student at LTH</p>
           </div>
           <div className="infoCard">
-            <p>Specialization towards Machine Learning and Software</p>
+            <h1>4.5+</h1>
+            <p>Grade point average (out of 5)</p>
           </div>
           <div className="infoCard">
-            <p>Passionate about learning and problem solving</p>
+            <h1>8</h1>
+            <p>Course offerings as a TA in programming courses</p>
           </div>
           <div className="infoCard">
-            <p>Some text</p>
+            <h1>150+</h1>
+            <p>Hours of tutoring computer science at LTH</p>
           </div>
+        </div>
+        <div className="section" id="contactButtons">
+          <button id="resumeButton">
+            <a
+              id="buttonContent"
+              href={cv}
+              download="Theodor_Anderberg_Torngren_CV" 
+              target='_blank'
+            >
+              <i className="material-icons">file_download</i>
+              <p> Download CV</p>
+            </a>
+          </button>
+          <button id="connectButton">
+            <a
+              id="buttonContent"
+              href="mailto:theodor.anderberg.torngren@gmail.com"
+            >
+              <i className="material-icons">mail_outline</i>
+              <p> Contact me</p>
+            </a>
+          </button>
         </div>
       </Container>
     </div>
